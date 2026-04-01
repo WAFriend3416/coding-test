05b0cd1999aa6033bc66498c7d4030faabd358d4
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-01 22:23:14 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main(){
+    cin >> N;
+    int long_num;
+    long_num = N/4;
+    for(int i=0; i<long_num; i++)cout << "long ";
+    cout << "int";
+    return 0;
+}
