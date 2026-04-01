71a6dfe6774982ca76187eee911a429624fa12cb
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-01 22:53:08 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    while(1){
+        int A,B;
+        cin >> A >> B;
+        if(A==0 && B==0) break;
+        cout << A+B << "\n";
+    }
+    return 0;
+}
