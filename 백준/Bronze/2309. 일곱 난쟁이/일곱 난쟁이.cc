29904083f74550ecb83e277574686d319df3f2e9
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-06 13:55:20 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,33 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 9;
-bool flag = false;
 vector<int> v;
-int num1,num2,sum=0;
 int main(){
-    for(int i=0; i<N; i++){
+    for(int i =0; i<N; i++){
         int n;
         cin >> n;
-        v.push_back(n);
-        sum += n;
+        v.push_back(n);        
     }
+    
     sort(v.begin(),v.end());
-    // 9개 중에서 2개를 뽑아서 합에서  뺐을때, 100이 되면 정지
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(i == j) continue;
-            if(sum - v[i] - v[j] == 100){
-                flag = true;
-                num1 = i;
-                num2 = j;
-                break;
-            }
+
+    do{
+        int sum = 0;
+        for(int j=0; j<7; j++){
+            sum += v[j];
         }
-        if(flag) break;
-    }
-    for(int k=0; k<N; k++){
-        if(k == num1 || k ==num2) continue;
-        cout << v[k] << "\n";
-    }
-    return 0;
+        if(sum == 100){
+            for(int j=0; j<7; j++){
+                cout << v[j] << "\n";
+            }    
+            break;
+        }
+    }while(next_permutation(v.begin(),v.end()));
 }
