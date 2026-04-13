75b21166ab55aa830a6409a6b5a6eb14033047d0
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-13 10:34:54 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,31 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+string s;
 int main(){
-    //cout << char(90+13) << " ";   
-    string s;
     getline(cin,s);
-    for(int i=0; i<s.size(); i++){
-        if(s[i] >= 65 && s[i] <= 90){
-            if(s[i] > 77){
-                s[i] -= 13;
-            }
-            else{
-                s[i] += 13;
-            }
+    for(char c : s){
+        if(c >= 97 && c<=122){ // 소문자
+            if(c >= 110) cout << char(c-13);
+            else cout << char(c+13);
         }
-        else if (s[i] >= 97 && s[i] <= 122){
-            if(s[i] > 109){
-                s[i] -= 13;
-            }
-            else{
-                s[i] += 13;
-            }
+        else if(c>=65 && c<=90){ // 대문자
+            if(c >= 78) cout << char(c-13);
+            else cout << char(c+13);
         }
         else{
-            continue;
+            cout << c;
         }
     }
-    cout << s << " ";
     return 0;
 }
