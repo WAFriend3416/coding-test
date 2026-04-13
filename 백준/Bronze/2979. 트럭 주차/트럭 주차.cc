3f4520fbdf822c7759f2cf9d449a00aa4ddc1eb2
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-13 10:05:49 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int a[101];
-int A,B,C,ans; // 1대 , 2대, 3대
+int A,B,C,sum;
 int main(){
     cin >> A >> B >> C;
     for(int i=0; i<3; i++){
-        int s,e;
-        cin >> s >> e;
-        for(int j=s; j<e; j++){
+        int S,E;
+        cin >> S >> E;
+        for(int j = S; j<E; j++){
             a[j]++;
         }
     }
-    
-    for(auto k : a){
-        if (k==1) ans += (k*A);
-        else if (k==2) ans += (k*B);
-        else if (k==3) ans += (k*C);
-        else continue;
+    for(int k : a){
+        if(k == 1) sum += (k*A);
+        else if(k == 2) sum += (k*B);
+        else sum += (k*C);
     }
-    cout << ans ;
+    cout << sum;
     return 0;
 }
