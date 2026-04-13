9df553bf564bfde02ce1f07e5049de228709345a
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-13 11:12:51 +0900

    [Silver III] Title: 수열, Time: 28 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N,M;
 int a[100005];
 int ps[100005];
-int N,K;
 int main(){
-    cin >> N >> K;
+    int max = -10000000;
+    cin >> N >> M;
     for(int i=1; i<=N; i++){
-        int n;
         cin >> a[i];
-        ps[i] += ps[i-1] + a[i];
+        ps[i] = ps[i-1]+a[i];
     }
-    int MAX = ps[K];
-    for(int j=K; j<=N; j++){
-        int sum = ps[j] - ps[j-K];
-        if(MAX < sum) MAX = sum;
+    for(int i=M; i<=N; i++){
+        int sum = ps[i] - ps[i-M];
+        if (max < sum) max = sum;
     }
-    cout << MAX;
+    cout << max;
     return 0;
 }
