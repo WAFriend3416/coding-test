090bbe5c71f65f09b8719026b9471a54690d2dd9
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-20 12:50:31 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    string tmp1,tmp2;
+    tmp1 = to_string(a) + to_string(b);
+    tmp2 = to_string(b) + to_string(a);
+    if(stoi(tmp1) > stoi(tmp2)) return stoi(tmp1);
+    else return stoi(tmp2);
+}
