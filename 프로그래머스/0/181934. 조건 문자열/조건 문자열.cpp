2cfa02285003a534133feb2913af1280f09eaf31
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-24 14:07:38 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    if(eq == "="){
+        if(ineq == "<") return n <= m;
+        else if(ineq == ">") return n >= m;
+    }
+    else{
+        if(ineq == "<") return n < m;
+        else if(ineq == ">") return n > m;
+    }
+    return answer;
+}
