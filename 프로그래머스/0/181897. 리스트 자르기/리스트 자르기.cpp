6a30ff8049372cc75336976de53f85d3daa0e863
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-24 12:06:26 +0900

    [level 0] Title: 리스트 자르기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
+    vector<int> answer;
+    int start,end,step=1;
+    if(n == 1){
+        start = 0;
+        end = slicer[1];
+    }
+    else if(n == 2){
+        start = slicer[0];
+        end = num_list.size()-1;
+    }
+    else if(n == 3){
+        start = slicer[0];
+        end = slicer[1];
+    }
+    else{
+        start = slicer[0];
+        end = slicer[1];
+        step = slicer[2];
+    }
+    for(int i = start; i<=end; i+=step){
+        answer.push_back(num_list[i]);
+    }
+    return answer;
+}
