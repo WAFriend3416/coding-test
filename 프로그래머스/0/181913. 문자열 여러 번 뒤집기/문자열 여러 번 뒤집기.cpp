937a1d28fc0c86a78fb8d722f9de17b1a6dbf175
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-24 13:57:36 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 0.19 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string, vector<vector<int>> queries) {
+    string answer = "";
+    for(auto v : queries){
+        reverse(my_string.begin()+v[0],my_string.begin()+v[1]+1);
+    }
+    return my_string;
+}
