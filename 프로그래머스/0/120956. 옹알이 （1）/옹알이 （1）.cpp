d359094268bbf6ae27db1921a3db5a3ca99ca3a4
WAFriend3416 <127187396+WAFriend3416@users.noreply.github.com>
2026-04-29 21:25:31 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+    for(string s : babbling){
+        int start = 0;
+        bool flag = true;
+        if(s.find("aya") != string::npos){
+            s.replace(s.find("aya"), 3, " ");
+        }
+        if(s.find("ye") != string::npos){
+            s.replace(s.find("ye"), 2, " ");
+        }
+        if(s.find("woo") != string::npos){
+            s.replace(s.find("woo"), 3, " ");
+        }
+        if(s.find("ma") != string::npos){
+            s.replace(s.find("ma"), 2, " ");
+        }
+        
+        for(char c: s){
+            if(c != ' ')flag = false;
+        }
+        if(flag)answer++;
+    }
+    return answer;
+}
